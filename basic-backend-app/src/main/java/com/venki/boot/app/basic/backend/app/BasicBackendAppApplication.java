bd36b176c8ec008a3e38f3b50cf9716fa6e6a503
venkat-streben <venky4026@gmail.com>
2024-01-29 16:47:30 +0530

    baisc structure

---
+++ basic-backend-app/src/main/java/com/venki/boot/app/basic/backend/app/BasicBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.venki.boot.app.basic.backend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBackendAppApplication.class, args);
+	}
+
+}
---
+++ basic-backend-app/src/test/java/com/venki/boot/app/basic/backend/app/BasicBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venki.boot.app.basic.backend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
